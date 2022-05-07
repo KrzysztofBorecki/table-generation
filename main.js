3314b55257fb00d2f161678dc94bf42552ef68ce
Krzysztof Borecki <80486337+K3orecki@users.noreply.github.com>
2022-05-07 16:02:36 +0200

    Refactor variables names and usage

--- main.js
+++ main.js
@@ -1,21 +1,42 @@
 'use strict';
 
 (function createTable() {
+    const SELECTED_COLUMN_CLASS = 'selected-column';
+    const SELECTED_ROW_CLASS = 'selected-row';
+    const TABLE_CELL_CLASS = 'table-cell';
+    const TABLE_BODY_CLASS = 'table-body';
+    const TABLE_CONTAINER_CLASS = 'table-container';
+    const TABLE_CLASS = 'table';
+    const SECTION_CLASS = 'section';
+    const HIDDEN_CLASS = 'hidden';
+    const ERROR_VALIDATION_CLASS = 'validationError';
+    const ERROR_BOX_VALIDATION_CLASS = 'validationErrorBox';
+    const NUMBER_OF_ROWS_FIELD_ID = 'number-of-rows';
+    const NUMBER_OF_COLUMNS_FIELD_ID = 'number-of-columns';
+    const SELECTED_ROW_FIELD_ID = 'selected-row';
+    const SELECTED_COLUMN_FIELD_ID = 'selected-column';
+    const FORM_FIELDS = [NUMBER_OF_ROWS_FIELD_ID, NUMBER_OF_COLUMNS_FIELD_ID, SELECTED_ROW_FIELD_ID, SELECTED_COLUMN_FIELD_ID];
+    const FORBIDDEN_KEYS = ['ArrowLeft', 'ArrowRight', 'Delete', 'Backspace', 'Tab'];
+
     const formFields = getFormFields();
 
+    function getParsedFieldName(fieldName) {
+        return fieldName.split('-').map((word, idx) => {
+            if (!idx) return word;
+
+            return `${word[0].toUpperCase()}${word.slice(1)}`;
+        }).join('');
+    }
+
     function getFormFields() {
-        return [
-            'numOfRows',
-            'numOfColumns',
-            'selectedRow',
-            'selectedColumn'
-        ].reduce((formFields, fieldName) => {
+        return FORM_FIELDS.reduce((formFields, fieldName) => {
             const formField = {};
+            const parsedFieldName = getParsedFieldName(fieldName);
 
             formField.element = document.getElementById(fieldName);
             formField.parsedValue = parseInt(document.getElementById(fieldName).value);
 
-            formFields[fieldName] = formField;
+            formFields[parsedFieldName] = formField;
 
             return formFields;
         }, {});
@@ -25,7 +46,7 @@
         if (
             ((e.target.value === '' || e.target.value === '0') && e.key === '0')
             || (!((parseInt(e.key) <= 9) 
-            || ['ArrowLeft', 'ArrowRight', 'Delete', 'Backspace', 'Tab'].includes(e.key)))
+            || FORBIDDEN_KEYS.includes(e.key)))
         ) {
             e.preventDefault();
         }
@@ -35,132 +56,123 @@
         element.remove();
     }
 
-    function resetFields() {
-        const selectionFields = [formFields.selectedRow, formFields.selectedColumn];
-
+    function resetFields(...selectionFields) {
+        console.log(selectionFields);
         selectionFields.forEach(field => field.value = '');
     }
 
-    function addNewTable(formFields) {
-        function createCellValue(rowIdx, colIdx) {
-            return `${rowIdx + 1}${colIdx + 1}`;
-        }
-
-        function createDataCell(rowIdx, colIdx) {
-            const dataCell = document.createElement('td');
-
-            dataCell.classList = 'row-cell'; 
-            dataCell.innerHTML = createCellValue(rowIdx, colIdx);
-
-            return dataCell;
-        }
-
-        function createRow(rowIdx) {
-            const declaredNumOfColumns = formFields.numOfColumns.parsedValue;
-            const declaredSelectedColumn = formFields.selectedColumn.parsedValue;
-
-            const rowElement = Array(declaredNumOfColumns)
-                .fill(null)
-                .map((_, colIdx) => {
-                    const dataCell = createDataCell(rowIdx, colIdx);
-
-                    if (declaredSelectedColumn && colIdx === declaredSelectedColumn - 1) {
-                        dataCell.classList.add('selected-column');
-                    }
-
-                    return dataCell;
-                });
-
-            return rowElement;
-        } 
-
-        function createRows() {
-            const declaredNumOfRows = formFields.numOfRows.parsedValue;
-            const declaredSelectedRow = formFields.selectedRow.parsedValue;
-
-            const rowElements = Array(declaredNumOfRows)
-                .fill(null)
-                .map((_, rowIdx) => {
-                    const newRow = document.createElement('tr');
-                    newRow.classList = 'table-row';
-
-                    const rowElement = createRow(rowIdx);
-                    newRow.append(...rowElement);
-
-                    const clonedElement = newRow.cloneNode(true);
-
-                    if (declaredSelectedRow && rowIdx === declaredSelectedRow - 1) {
-                        clonedElement.classList.add('selected-row');
-                    }
-
-                    return clonedElement;
-                });
-
-            return rowElements;
-        }
-
-        function createTBody() {
-            const rowElements = createRows();
-            const newTBody = document.createElement('tbody');
-
-            newTBody.classList = 'table-body';
-            newTBody.append(...rowElements);
-
-            return newTBody;
-        }
-
-        function createTable() {
-            const newTBody = createTBody();
-            const newTable = document.createElement('table');
-
-            newTable.classList = 'table';
-            newTable.append(newTBody);
-
-            return newTable;
-        }
-
-        function createTableCont() {
-            const newTable = createTable();
-            const newTableCont = document.createElement('div');
-
-            newTableCont.classList = 'table-container';
-            newTableCont.append(newTable);
-
-            return newTableCont;
-        }
-
-        function appendTable() {
-            const newTableCont = createTableCont();
-
-            document.querySelector('section').append(newTableCont);
-        }
-
-        appendTable();
+    function createCellValue(rowIdx, colIdx) {
+        return `${rowIdx + 1}${colIdx + 1}`;
     }
 
-    function removeValidationErrorStyles(validationErrorElements) {
-        Array.from(validationErrorElements)
-            .forEach((field) => field.classList.remove('validationError'));
+    function createTableCell(rowIdx, colIdx) {
+        const tableCell = document.createElement('td');
 
-        document.querySelector('.validationErrorBox').classList.add('hidden');
+        tableCell.classList = TABLE_CELL_CLASS; 
+        tableCell.innerHTML = createCellValue(rowIdx, colIdx);
+
+        return tableCell;
     }
 
-    function addValidationErrorStyles(formFields) {
-        const declaredNumOfRows = formFields.numOfRows.parsedValue;
-        const declaredNumOfColumns = formFields.numOfColumns.parsedValue;
-        const declaredSelectedRow = formFields.selectedRow.parsedValue;
-        const declaredSelectedColumn = formFields.selectedColumn.parsedValue;
+    function createTableCells(numberOfColumns, selectedColumn, rowIdx) {
+        const tableCells = Array(numberOfColumns)
+            .fill(null)
+            .map((_, colIdx) => {
+                const tableCell = createTableCell(rowIdx, colIdx);
 
-        if (declaredSelectedRow > declaredNumOfRows) {
-            formFields.selectedRow.element.classList.add('validationError');
+                if (selectedColumn && colIdx === selectedColumn - 1) {
+                    tableCell.classList.add(SELECTED_COLUMN_CLASS);
+                }
+
+                return tableCell;
+            });
+
+        return tableCells;
+    } 
+
+    function createTableRows(numberOfRows, numberOfColumns, selectedRow, selectedColumn) {
+        const tableRows = Array(numberOfRows)
+            .fill(null)
+            .map((_, rowIdx) => {
+                const tableRow = document.createElement('tr');
+                const tableCells = createTableCells(numberOfColumns, selectedColumn, rowIdx);
+
+                tableRow.append(...tableCells);
+
+                const clonedRow = tableRow.cloneNode(true);
+
+                if (selectedRow && rowIdx === selectedRow - 1) {
+                    clonedRow.classList.add(SELECTED_ROW_CLASS);
+                }
+
+                return clonedRow;
+            });
+
+        return tableRows;
+    }
+
+    function createTableBody(numberOfRows, numberOfColumns, selectedRow, selectedColumn) {
+        const tableRows = createTableRows(numberOfRows, numberOfColumns, selectedRow, selectedColumn);
+        const tableBody = document.createElement('tbody');
+
+        tableBody.classList = TABLE_BODY_CLASS;
+        tableBody.append(...tableRows);
+
+        return tableBody;
+    }
+
+    function createTable(numberOfRows, numberOfColumns, selectedRow, selectedColumn) {
+        const tableBody = createTableBody(numberOfRows, numberOfColumns, selectedRow, selectedColumn);
+        const table = document.createElement('table');
+
+        table.classList = TABLE_CLASS;
+        table.append(tableBody);
+
+        return table;
+    }
+
+    function createTableContainer(numberOfRows, numberOfColumns, selectedRow, selectedColumn) {
+        const table = createTable(numberOfRows, numberOfColumns, selectedRow, selectedColumn);
+        const tableContainer = document.createElement('div');
+
+        tableContainer.classList = TABLE_CONTAINER_CLASS;
+        tableContainer.append(table);
+
+        return tableContainer;
+    }
+
+    function appendTable(numberOfRows, numberOfColumns, selectedRow, selectedColumn) {
+        const tableContainer = createTableContainer(numberOfRows, numberOfColumns, selectedRow, selectedColumn);
+
+        document.querySelector(SECTION_CLASS).append(tableContainer);
+    }
+
+    function addTable(numberOfRows, numberOfColumns, selectedRow, selectedColumn) {
+        appendTable(numberOfRows, numberOfColumns, selectedRow, selectedColumn);
+    }
+
+    function removeValidationErrorStyles(validationErrorFields) {
+        Array.from(validationErrorFields)
+            .forEach((field) => field.classList.remove(ERROR_VALIDATION_CLASS));
+
+        document.querySelector(`.${ERROR_BOX_VALIDATION_CLASS}`).classList.add(HIDDEN_CLASS);
+    }
+
+    function addValidationErrorStyles(
+            numberOfRows, numberOfColumns, 
+            selectedRow, selectedColumn, 
+            selectedRowElement, selectedColumnElement
+        ) {
+        if (selectedRow > numberOfRows) {
+            selectedRowElement.classList.add(ERROR_VALIDATION_CLASS);
         }
 
-        if (declaredSelectedColumn > declaredNumOfColumns) {
-            formFields.selectedColumn.element.classList.add('validationError');
+        if (selectedColumn > numberOfColumns) {
+            selectedColumnElement.classList.add(ERROR_VALIDATION_CLASS);
         }
 
-        if (document.querySelector('.validationError')) {
-            document.querySelector('.validationErrorBox').classList.remove('hidden');
+        if (document.querySelector(`.${ERROR_VALIDATION_CLASS}`)) {
+            document.querySelector(`.${ERROR_BOX_VALIDATION_CLASS}`).classList.remove(HIDDEN_CLASS);
         }
     }
 
@@ -168,60 +180,79 @@
         selector.setAttribute('placeholder', value);
     }
 
-    function setPlaceholders(formFields) {
-        const numOfRows = formFields.numOfRows.element.value;
-        const numOfColumns = formFields.numOfColumns.element.value;
+    function setPlaceholders(
+            numberOfRows, numberOfColumns, 
+            selectedRowElement, selectedColumnElement
+        ) {
+        const selectedRowPlaceholder = numberOfRows > 0 ? `value 1 - ${numberOfRows}` : 'value > 0';
+        const selectedColumnPlaceholder = numberOfColumns > 0 ? `value 1 - ${numberOfColumns}` : 'value > 0';
 
-        const selectedRowPlaceholder = numOfRows > 0 ? `value 1 - ${numOfRows}` : 'value > 0';
-        const selectedColumnPlaceholder = numOfColumns > 0 ? `value 1 - ${numOfColumns}` : 'value > 0';
-
-        setPlaceholder(formFields.selectedRow.element, selectedRowPlaceholder);
-        setPlaceholder(formFields.selectedColumn.element, selectedColumnPlaceholder);
+        setPlaceholder(selectedRowElement, selectedRowPlaceholder);
+        setPlaceholder(selectedColumnElement, selectedColumnPlaceholder);
     }
 
     function disableSelectionField(field, value) {
         field.disabled = value;
     }
 
-    function disableSelectionFields(formFields) {
-        const declaredNumOfRows = formFields.numOfRows.parsedValue;
-        const declaredNumOfColumns = formFields.numOfColumns.parsedValue;
-        const declaredSelectedRow = formFields.selectedRow.parsedValue;
-        const declaredSelectedColumn = formFields.selectedColumn.parsedValue;
+    function disableSelectionFields(
+            numberOfRows, numberOfColumns, 
+            selectedRow, selectedColumn, 
+            selectedRowElement, selectedColumnElement
+        ) {
+        const selectedRowDisabledStatus = numberOfRows > 0 || selectedRow > 0;
+        const selectedColumnDisabledStatus = numberOfColumns > 0 || selectedColumn > 0;
 
-        const selectedRowDisabledStatus = declaredNumOfRows > 0 || declaredSelectedRow > 0;
-        const selectedColumnDisabledStatus = declaredNumOfColumns > 0 || declaredSelectedColumn > 0;
-
-        disableSelectionField(formFields.selectedRow.element, !selectedRowDisabledStatus);
-        disableSelectionField(formFields.selectedColumn.element, !selectedColumnDisabledStatus);
+        disableSelectionField(selectedRowElement, !selectedRowDisabledStatus);
+        disableSelectionField(selectedColumnElement, !selectedColumnDisabledStatus);
     } 
 
     function handleFormChange() {
-        const tableElement = document.querySelector('.table-container');
-        const validationErrorElements = document.querySelectorAll('.validationError');
+        const table = document.querySelector(`.${TABLE_CONTAINER_CLASS}`);
+        const validationErrorFields = document.querySelectorAll(`.${ERROR_VALIDATION_CLASS}`);
         const formFields = getFormFields();
-        const declaredNumOfRows = formFields.numOfRows.parsedValue;
-        const declaredNumOfColumns = formFields.numOfColumns.parsedValue;
-
-        if (tableElement) {
-            removeElement(tableElement);
+        const numberOfRows = formFields.numberOfRows.parsedValue;
+        const numberOfColumns = formFields.numberOfColumns.parsedValue;
+        const selectedRow = formFields.selectedRow.parsedValue;
+        const selectedColumn = formFields.selectedColumn.parsedValue;
+        const selectedRowElement = formFields.selectedRow.element;
+        const selectedColumnElement = formFields.selectedColumn.element;
+        
+        if (table) {
+            removeElement(table);
         }
 
-        if (!(declaredNumOfRows || declaredNumOfColumns)) {
-            resetFields();
+        if (!(numberOfRows || numberOfColumns)) {
+            resetFields(selectedRowElement, selectedColumnElement);
         } 
 
-        if (declaredNumOfRows && declaredNumOfColumns) {
-            addNewTable(formFields);
-        }  
-
-        if (validationErrorElements.length) {
-            removeValidationErrorStyles(validationErrorElements);
+        if (numberOfRows && numberOfColumns) {
+            addTable(
+                numberOfRows, numberOfColumns, 
+                selectedRow, selectedColumn
+            );
         }
 
-        addValidationErrorStyles(formFields);
-        setPlaceholders(formFields);
-        disableSelectionFields(formFields);
+        if (validationErrorFields.length) {
+            removeValidationErrorStyles(validationErrorFields);
+        }
+
+        addValidationErrorStyles(
+            numberOfRows, numberOfColumns, 
+            selectedRow, selectedColumn, 
+            selectedRowElement, selectedColumnElement
+        );
+
+        setPlaceholders(
+            numberOfRows, numberOfColumns, 
+            selectedRowElement, selectedColumnElement
+        );
+
+        disableSelectionFields(
+            numberOfRows, numberOfColumns, 
+            selectedRow, selectedColumn, 
+            selectedRowElement, selectedColumnElement
+        );
     }
 
     Object.values(formFields).forEach(field => field.element.addEventListener('keydown', hasClickedForbiddenKey));
